d6dd8e658cbf9be7de4a24e9cb51ec2cd1e7b613
xuzhe <592717159@qq.com>
2017-01-20 10:59:18 +0800

    jizhensuanfa

---
+++ 矩阵算法.py
@@ -0,0 +1,13 @@
+def determinant(matrix):
+    result = 0
+    l = len(matrix)
+    if l == 1:
+        return matrix[0][0]
+    if l == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    for j in range(0, l):
+        if l!=2:
+            sub_matrix = []               
+            sub_matrix = [(row[0:j]+row[j+1:]) for row in matrix[1:]]
+        result = result + (-1)**j * matrix[0][j] * determinant(sub_matrix)
+    return result
